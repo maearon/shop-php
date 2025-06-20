9e5ada90bb0ecfa62567417a79f22f0d4cf707f3
maearon <manhng132@gmail.com>
2025-06-20 15:43:08 +0700

    Refactor code structure for improved readability and maintainability

--- apps/web/components/shared/api/index.tsx
+++ apps/web/components/shared/api/index.tsx
@@ -2,9 +2,9 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 let BASE_URL = ''
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://localhost:9000/api'
+  BASE_URL = 'http://localhost:3000/api'
 } else {
-  BASE_URL = 'http://localhost:9000/api'
+  BASE_URL = 'http://localhost:3000/api'
 }
 
 axios.defaults.xsrfCookieName = 'CSRF-TOKEN';
