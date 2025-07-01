0b76dffedd88bc8788e377238a7dc88161f1d681
maearon <manhng132@gmail.com>
2025-07-01 10:20:48 +0700

    wip: HEAD /up api csharp

--- apps/api-gateway/server.js
+++ apps/api-gateway/server.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const gateway = require('express-gateway');
-const express = require('express');
-
-const app = express();
-
-// ✅ HEAD /up route
-app.head('/up', (req, res) => {
-  res.status(200).end();
-});
 
 gateway()
   .load(path.join(__dirname, 'config'))
-  .run(app);
+  .run();
