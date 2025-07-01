17e94edcf5e59c6996e853c3d3b77c8679658d4d
maearon <manhng132@gmail.com>
2025-07-01 13:25:56 +0700

    fix cors gateway

--- apps/api-gateway/server.js
+++ apps/api-gateway/server.js
@@ -1,6 +1,20 @@
-const gateway = require('express-gateway');
+const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const gateway = require('express-gateway');
+
+const app = express();
+
+const corsOptions = {
+  origin: "https://adidas-mocha.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 
 gateway()
   .load(path.join(__dirname, 'config'))
-  .run();
+  .run(app);
