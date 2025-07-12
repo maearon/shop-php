a8ab436b5fa935311a9856807eb5b564f0158e1e
maearon <manhng132@gmail.com>
2025-07-12 22:12:24 +0700

    Logo Loading Larger and Fill auto resize

--- apps/web/components/ui/FullScreenLoader.tsx
+++ apps/web/components/ui/FullScreenLoader.tsx
@@ -1,29 +1,3 @@
-// app/components/ui/FullScreenLoader.tsx
-// "use client"
-
-// import React from "react"
-// import Image from "next/image"
-
-// const FullScreenLoader = () => {
-//   return (
-//     <div
-//       className="fixed inset-0 z-50 flex items-center justify-center bg-white"
-//       data-auto-id="loading-screen"
-//     >
-//       <div className="opacity-10 animate-pulse">
-//         <Image
-//           src="/adidas-logo.svg" // 👈 bạn phải đặt logo 3 sọc ở public/adidas-logo.svg
-//           alt="Loading"
-//           width={80}
-//           height={80}
-//           priority
-//         />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default FullScreenLoader
 "use client"
 
 import React from "react"
@@ -31,16 +5,16 @@ import Image from "next/image"
 
 export default function FullScreenLoader() {
   return (
-    <div className="fixed inset-0 z-[9999] bg-white flex items-center justify-center">
-      <div className="relative w-20 h-20 animate-pulse opacity-70">
-        {/* <div className="absolute top-0 left-0 w-3 h-full bg-black transform rotate-12 origin-bottom-left"></div>
-        <div className="absolute top-0 left-4 w-3 h-full bg-black transform rotate-12 origin-bottom-left"></div>
-        <div className="absolute top-0 left-8 w-3 h-full bg-black transform rotate-12 origin-bottom-left"></div> */}
+    <div
+      className="fixed inset-0 z-[9999] bg-white flex items-center justify-center"
+      data-auto-id="loading-screen"
+    >
+      <div className="relative w-28 h-28 animate-pulse opacity-80">
         <Image
           src="/logo.png"
           alt="Loading"
-          width={80}
-          height={80}
+          fill
+          className="object-contain"
           priority
         />
       </div>
